a8f20a53cfe29414d6803374cb8766ab8511d52e
terechenko akim <anddrey2703@gmail.com>
2024-04-04 19:36:19 +0300

    sait

--- screapt.js
+++ screapt.js
@@ -1,48 +1,62 @@
 let PositionData = {}
-const movePawn =(index, side, isLeft)=>{
-    const innerMovePawn = () =>{
-        alert (`Пешка №${index} цвет ${side} идет на  ${isLeft ?'лево' :'право'}`)
+const movePawn = (index, side, isLeft) => {
+    const innerMovePawn = () => {
+        alert(`Пешка №${index} цвет ${side} идет на  ${isLeft ? 'лево' : 'право'}`)
     }
     return innerMovePawn
 }
-const backlightLeftParent=document.getElementById("backlight_left_parent");
-const backlightRightParent=document.getElementById('backlight_right_parent');
+const backlightLeftParent = document.getElementById("backlight_left_parent");
+const backlightRightParent = document.getElementById('backlight_right_parent');
 window.addEventListener('load', () => {
-    var backlight_left = document.getElementById("backlight_left");
-    var backlight_right = document.getElementById("backlight_right");
-    var white = document.getElementsByClassName('white');
-    var belie_i_chernie_chtyki = document.getElementsByClassName("big");
+    const backlight_left = document.getElementById("backlight_left");
+    const backlight_right = document.getElementById("backlight_right");
+    const white = document.getElementsByClassName('white');
     const backlight_up = (x, y, side, i) => {
         console.log(PositionData);
         var backlight_left = document.getElementById("backlight_left");
         var backlight_right = document.getElementById("backlight_right");
         const difference = side === 'white' ? -1 : 1;
         const isHasRow = (Number(y) + difference) in PositionData
-        const isLeftHas = isHasRow && ((Number (x) -1 ) in PositionData[Number(y) + difference])
-        const isRightHas = isHasRow && ((Number (x) + 1) in PositionData[Number(y) + difference])  
+        const isLeftHas = isHasRow && ((Number(x) - 1) in PositionData[Number(y) + difference])
+        const isRightHas = isHasRow && ((Number(x) + 1) in PositionData[Number(y) + difference])
+
         if (x > 1 && !isLeftHas) {
             const backlight_left_copy = backlight_left.cloneNode(true)
-            backlight_left_parent.replaceChild(backlight_left_copy,backlight_left)
+            backlight_left_copy.setAttribute('data-x', String(Number(x) - 1));
+            backlight_left_copy.setAttribute('data-y', String(Number(y) + difference));
+            backlight_left_parent.replaceChild(backlight_left_copy, backlight_left)
             backlight_left_copy.style.display = 'block';
             backlight_left_copy.style.left = (x - 2) * 100 + '%';
             backlight_left_copy.style.top = (Number(y) + difference + -1) * 100 + "%";
-            backlight_left_copy.addEventListener('click',movePawn(i, side, true))
+            backlight_left_copy.addEventListener('click', movePawn(i, side, true))
+            delete PositionData[Number(y)][Number(x)]
+            if (!((Number(y) + difference) in PositionData)) {
+                PositionData[Number(y) + difference] = {}
+            }
+            PositionData[Number(y) + difference][ Number (x) -1] = side === 'black'? true: false;
         }
         else {
             backlight_left.style.display = 'none';
         }
         if (x < 8 && !isRightHas) {
+            delete PositionData[Number(y)][Number(x)]
             const backlight_right_copy = backlight_right.cloneNode(true)
-            backlightRightParent.replaceChild(backlight_right_copy,backlight_right)
+            backlight_right_copy.setAttribute('data-x', String(Number(x) + 1));
+            backlight_right_copy.setAttribute('data-y', String(Number(y) + difference));
+            backlightRightParent.replaceChild(backlight_right_copy, backlight_right)
             backlight_right_copy.style.display = 'block';
             backlight_right_copy.style.left = (x - 2) * 100 + '%';
             backlight_right_copy.style.top = (Number(y) + difference + -1) * 100 + "%";
-            backlight_right_copy.addEventListener('click',movePawn(i, side, false))
+            backlight_right_copy.addEventListener('click', movePawn(i, side, false))
+            if (!((Number(y) + difference) in PositionData)) {
+                PositionData[Number(y) + difference] = {}
+            }
+            PositionData[Number(y) + difference][ Number (x) +1] = side === 'black'? true: false;
         }
         else {
             backlight_right.style.display = 'none';
         }
-
+        console.log(PositionData);
         //alert(${x} ${y} ${side})
     }
     const backlight_off = () => {
