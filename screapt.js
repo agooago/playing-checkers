6605028d74dbbede36c6fdba50d6f5f4a840466a
terechenko akim <anddrey2703@gmail.com>
2024-05-30 20:09:44 +0300

    определяют где белые и черные

--- screapt.js
+++ screapt.js
@@ -58,35 +58,69 @@ const backlightRightParent = document.getElementById('backlight_right_parent');
 window.addEventListener('load', () => {
     const backlight_left = document.getElementById("backlight_left");
     const backlight_right = document.getElementById("backlight_right");
-    const white = document.getElementsByClassName('white');
     const backlight_up = (side, i) => {
         const currentPawn = document.getElementById(`${side}${i}`)
-        const x = currentPawn.getAttribute('data-x');
-        const y = currentPawn.getAttribute('data-y');
+        const x = Number(currentPawn.getAttribute('data-x'));
+        const y = Number(currentPawn.getAttribute('data-y'));
+        const difference = side === 'white' ? -1 : 1;
+        const nextLeftX = x - 1;
+        const nextRightX = x + 1;
+        const nextY = y + difference;
+        if (nextY in PositionData) {
+            console.log('На следующей строчки есть пешки')
+            if (nextLeftX in PositionData[nextY] && PositionData[nextY][nextLeftX] !== undefined) {
+                console.log('На следующей строчке есть пешка слева')
+                const nextPawnSide = PositionData[nextY][nextLeftX] ? 'black' : 'white';
+                console.log(`пешка на следующей строчке слева ${nextPawnSide}`)
+                if (nextPawnSide !== side) {
+                    console.log('Эта пешка наш противник!');
+                    const nextNextX = nextLeftX - 1;
+                    const nextNextY = nextY + difference;
+                    console.log(`Мы попробуем сеъсть её и перемиститься на кординаты ${nextNextX} ${nextNextY}`)
+                    if (!(nextNextY in PositionData) ||!(nextNextX in PositionData[nextNextY])|| PositionData[nextNextY][nextNextX] === undefined && nextLeftX > 0 && nextLeftX < 9 && nextNextY > 0 && nextNextY < 9) {
+                        console.log('Можем есть влево')
+                    }
+                }
+            }
+            if (nextRightX in PositionData[nextY] && PositionData[nextY][nextRightX] !== undefined) {
+                console.log('На следующей строчке есть пешка справа')
+                const nextPawnSide = PositionData[nextY][nextRightX] ? 'black' : 'white';
+                console.log(`пешка на следующей строчке справа ${nextPawnSide}`)
+                if (nextPawnSide !== side) {
+                    console.log('Эта пешка наш противник!');
+                    const nextNextX = nextRightX + 1;
+                    const nextNextY = nextY + difference;
+                    console.log(`Мы попробуем сеъсть её и перемиститься на кординаты ${nextNextX} ${nextNextY}`)
+                    if (!(nextNextY in PositionData) ||!(nextNextX in PositionData[nextNextY])|| PositionData[nextNextY][nextNextX] === undefined && nextLeftX > 0 && nextLeftX < 9 && nextNextY > 0 && nextNextY < 9) {
+                        console.log('Можем есть вправо')
+                    }
+                }
+            }
+}
         if (currentStep % 2 === 0 && side === 'black' || currentStep % 2 === 1 && side === 'white') {
             const pawn = document.getElementById(`${side}${i}`);
             var backlight_left = document.getElementById("backlight_left");
             var backlight_right = document.getElementById("backlight_right");
             const difference = side === 'white' ? -1 : 1;
-            const isHasRow = (Number(y) + difference) in PositionData
-            const isLeftHas = isHasRow && ((Number(x) - 1) in PositionData[Number(y) + difference])
-            const isRightHas = isHasRow && ((Number(x) + 1) in PositionData[Number(y) + difference])
+            const isHasRow = (y + difference) in PositionData
+            const isLeftHas = isHasRow && ((x - 1) in PositionData[y + difference])
+            const isRightHas = isHasRow && ((x + 1) in PositionData[y + difference])
 
             if (x > 1 && !isLeftHas) {
                 const backlight_left_copy = backlight_left.cloneNode(true)
                 backlightLeftParent.replaceChild(backlight_left_copy, backlight_left)
                 backlight_left_copy.style.display = 'block';
                 backlight_left_copy.style.left = (x - 2) * 100 + '%';
-                backlight_left_copy.style.top = (Number(y) + difference + -1) * 100 + "%";
+                backlight_left_copy.style.top = (y + difference + -1) * 100 + "%";
                 backlight_left_copy.addEventListener('click', () => {
-                    pawn.setAttribute('data-x', String(Number(x) - 1));
-                    pawn.setAttribute('data-y', String(Number(y) + difference));
+                    pawn.setAttribute('data-x', String(x - 1));
+                    pawn.setAttribute('data-y', String(y + difference));
                     movePawn(i, side, true)
-                    delete PositionData[Number(y)][Number(x)]
-                    if (!((Number(y) + difference) in PositionData)) {
-                        PositionData[Number(y) + difference] = {}
+                    delete PositionData[y][x]
+                    if (!((y + difference) in PositionData)) {
+                        PositionData[y + difference] = {}
                     }
-                    PositionData[Number(y) + difference][Number(x) - 1] = side === 'black' ? true : false;
+                    PositionData[y + difference][x - 1] = side === 'black' ? true : false;
                 })
 
             }
@@ -94,36 +128,27 @@ window.addEventListener('load', () => {
                 backlight_left.style.display = 'none';
             }
             if (x < 8 && !isRightHas) {
-                delete PositionData[Number(y)][Number(x)]
+                delete PositionData[y][x]
                 const backlight_right_copy = backlight_right.cloneNode(true)
                 backlightRightParent.replaceChild(backlight_right_copy, backlight_right)
                 backlight_right_copy.style.display = 'block';
                 backlight_right_copy.style.left = (x - 2) * 100 + '%';
-                backlight_right_copy.style.top = (Number(y) + difference + -1) * 100 + "%";
+                backlight_right_copy.style.top = (y + difference + -1) * 100 + "%";
                 backlight_right_copy.addEventListener('click', () => {
-                    pawn.setAttribute('data-x', String(Number(x) + 1));
-                    pawn.setAttribute('data-y', String(Number(y) + difference));
+                    pawn.setAttribute('data-x', String(x + 1));
+                    pawn.setAttribute('data-y', String(y + difference));
                     movePawn(i, side, false)
-                    if (!((Number(y) + difference) in PositionData)) {
-                        PositionData[Number(y) + difference] = {}
+                    if (!((y + difference) in PositionData)) {
+                        PositionData[y + difference] = {}
                     }
-                    PositionData[Number(y) + difference][Number(x) + 1] = side === 'black' ? true : false;
+                    PositionData[y + difference][x + 1] = side === 'black' ? true : false;
                 })
 
             }
             else {
                 backlight_right.style.display = 'none';
             }
-            console.log(PositionData);
         }
-        //alert(${x} ${y} ${side})
-    }
-    const backlight_off = () => {
-        backlight_left.style.display = 'none';
-        backlight_right.style.display = 'none';
-    }
-    for (var i = 0, len = white.length; i < len; i++) {
-        white[i].addEventListener('click', backlight_off)
     }
     for (let i = 1; i < 13; i++) {
         const blackfigure = document.getElementById(`black${i}`);
@@ -144,9 +169,8 @@ window.addEventListener('load', () => {
         })
         const whitefigure = document.getElementById(`white${i}`);
         whitefigure.addEventListener('click', () => {
-            backlight_up('white',)
+            backlight_up('white', i)
         })
 
     }
-    console.log(PositionData)
 })
